ce8be3509a1b970d30c60889fdb37957568b319c
taib mahmoudi <taib.mahmoudi@esprit.tn>
2021-11-08 13:15:32 +0100

    initial dockerized project

---
+++ src/main/java/com/example/demo/services/IUserService.java
@@ -0,0 +1,15 @@
+package com.example.demo.services;
+
+import java.util.List;
+
+import com.example.demo.entities.User;
+
+public interface IUserService { 
+	 
+	List<User> retrieveAllUsers(); 
+	User addUser(User u);
+	void deleteUser(String id);
+	User updateUser(User u);
+	User retrieveUser(String id);
+
+} 
